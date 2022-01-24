54c3dd2920a54edc215e2d305697181636969729
Neeraj Kumar <neerajkmr1619@gmail.com>
2022-01-24 17:53:48 -0500

    Helloworld implementation for react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./style.css"
+import Button from "./Button"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+    return (
+        <div>
+         <h1 style={{textAlign:"Center"}}>Hello World</h1>
+         <Button title="App Store"></Button>
+         <Button title="Play Store"></Button>
+         {/*<button className="button">App Store</button>
+         <button className="button">Play Store</button>*/}
+        </div>
+       
+    );
 }
 
 export default App;
